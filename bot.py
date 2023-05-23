13f615aa0a8d25e0d4d07df02d0e5bcb9e3026fb
Xilver Kamui <112789454+xilverkamui@users.noreply.github.com>
2023-05-23 15:31:47 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,33 +1,66 @@
 import os
+import pymysql
 import feedparser
-import requests
-from telegram import Bot
+import telegram
+from telegram import ParseMode
+from dotenv import load_dotenv
 
-bot_token = os.environ.get('BOT_TOKEN')
-chat_ids = os.environ.get('CHAT_IDS').split(',')
-rss_urls = os.environ.get('RSS_URLS').split(',')
+load_dotenv()
 
-def post_new_feed_items():
-    last_fetched_item_id = os.environ.get('LAST_FETCHED_ITEM_ID')
+# Retrieve environment variables
+bot_token = os.getenv("BOT_TOKEN")
+chat_ids = os.getenv("CHAT_IDS").split(",")
+rss_urls = os.getenv("RSS_URLS").split(",")
+db_host = os.getenv("DB_HOST")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAME")
 
-    for rss_url in rss_urls:
-        feed = feedparser.parse(rss_url)
-        for entry in feed.entries:
-            item_id = entry.get('id')
-            if item_id == last_fetched_item_id:
-                break
-            
-            title = entry.get('title')
-            description = entry.get('description')
-            link = entry.get('link')
+# Connect to the database
+db = pymysql.connect(host=db_host, user=db_user, password=db_password, db=db_name)
+cursor = db.cursor()
 
-            message = f'<b>{title}</b>\n{description}\n{link}'
+# Get previous feed items from the database
+prev_items = []
+get_prev_items_query = "SELECT published_time, title FROM feed_items"
+cursor.execute(get_prev_items_query)
+prev_items_results = cursor.fetchall()
+for row in prev_items_results:
+    prev_items.append((row[0], row[1]))
+
+# Initialize the Telegram bot
+bot = telegram.Bot(token=bot_token)
+
+
+def post_to_telegram(chat_id, message):
+    bot.send_message(chat_id=chat_id, text=message, parse_mode=ParseMode.HTML, disable_web_page_preview=False)
+
+
+def save_feed_item(new_items):
+    insert_item_query = 
+    cursor.executemany('INSERT INTO feed_items (published_time, title) VALUES (%s, %s)',new_items)
+    db.commit()
+
+
+# Process each RSS feed URL
+new_items = []
+for rss_url in rss_urls:
+    feed = feedparser.parse(rss_url)
+    for entry in feed.entries:
+        published_time = entry.published_parsed
+        title = entry.title
+
+        # Check if the feed item is new
+        if (published_time, title) not in prev_items:
+            # Post to Telegram
+            message = f"<b>{title}</b>\n\n{entry.description}\n\n{entry.link}"
             for chat_id in chat_ids:
-                bot = Bot(token=bot_token)
-                bot.send_message(chat_id=chat_id, text=message, parse_mode='HTML')
+                post_to_telegram(chat_id, message)
+			
+			new_items.append(published_time, title)
+			
+# Save the new feed item
+save_feed_item(new_items)
 
-            # Update last fetched item ID
-            os.environ['LAST_FETCHED_ITEM_ID'] = item_id
-
-# Run the bot
-post_new_feed_items()
+# Close the database connection
+db.close()
